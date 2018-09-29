97d86b73de8f266b6ab2b6e65f791d1c86981673
Geraldo Sequeira <gcm.sequeira@gmail.com>
2018-09-28 22:27:15 -0300

    now with relationships in model Member

--- app/models/Member.rb
+++ app/models/Member.rb
@@ -1,3 +1,14 @@
 class Member < ApplicationRecord
     belongs_to :campaing
+
+    validates :name, :email, :campaign, presence: true
+
+    after_save :set_campaign_pending
+
+    #pixel
+
+    protected
+    def set_campaign_pending
+        self.campaign.update(status: :pending)
+    end
 end 
