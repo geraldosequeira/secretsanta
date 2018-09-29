ec70059cc4743f0bf39337c174c029a8745f8830
Geraldo Sequeira <gcm.sequeira@gmail.com>
2018-09-28 22:07:50 -0300

    Campaing model now with your relationships

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,19 @@
 class Campaign < ApplicationRecord
     belongs_to :user
-end
+    has_many :members, dependent: :destroy
+
+    enum status: [:pending, :finished]
+
+    validates :title, :description, :user, :status, presence: true
+
+    before_create :set_status
+    before_create :set_member
+
+    def set_staus
+        self.status = :pending
+    end
+
+    def set_member
+        self.members << Member.create(name: self.user.name, email: self.user.email)
+    end
+ende
