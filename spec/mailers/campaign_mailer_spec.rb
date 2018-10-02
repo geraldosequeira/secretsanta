0bd43935b1c5f968f0724049b6458f102e26748f
Geraldo Sequeira <gcm.sequeira@gmail.com>
2018-10-02 10:24:51 -0300

    now with test of email

--- spec/mailers/campaign_mailer_spec.rb
+++ spec/mailers/campaign_mailer_spec.rb
@@ -1,17 +1,31 @@
 require "rails_helper"
 
 RSpec.describe CampaignMailer, type: :mailer do
+  
   describe "raffle" do
-    let(:mail) { CampaignMailer.raffle }
 
-    it "renders the headers" do
-      expect(mail.subject).to eq("Raffle")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+    before do 
+      @campaign = create(:campaign)
+      @member = create(:member, campaign: @campaign) 
+      @friend = create(:member, campaign: @campaign)
+      @mail = CampaignMailer.raffle(@campaign, @member, @friend)
+    end
+    
+    it "headers are visible" do 
+      expect(@mail).to eq("Nosso Amigo Secreto: #{@campaign.title}")
+      expect(@mail.to).to eq([@member.email])
     end
 
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+    it "exist name of member in body" do
+      expect(@mail.body.encoded).to match(@member.name)
+    end
+
+    it "exist creator name of campaign in body" do
+      expect(@mail.boy).to match(@campaign.user.name)
+    end
+
+    it "exist an link of member to set email how open" do
+      expect(@mail.body.encoded).to match("/members/#{@member.token}/opened")
     end
   end
 
