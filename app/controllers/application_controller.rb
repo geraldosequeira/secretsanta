b7b16b9566ef43d5ae5645dfd017fca79e036fc3
Geraldo Sequeira <gcm.sequeira@gmail.com>
2018-10-04 17:24:57 -0300

    sets 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
+    
+    rescue_form ActiveRecord::RecordNotFound, :with => :render_404
+
+    def render_404
+        redirect_to main_app.root_url
+    end
 end
