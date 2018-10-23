76c0149490bef8f6dfbaf2602568d3c5d671be89
Geraldo Sequeira <gcm.sequeira@gmail.com>
2018-10-23 12:11:18 -0300

    now generate and sets token for member

--- app/models/Member.rb
+++ app/models/Member.rb
@@ -5,7 +5,15 @@ class Member < ApplicationRecord
 
     after_save :set_campaign_pending
 
-    #pixel
+    def set_pixel
+        self.open = false
+        self.token = loop do
+            random_token = SecureRandom.urlsafe_base64(nil, false)
+            break random_token unless Member.exists?(token: random_token)
+        end
+        self.save!
+    end
+    
 
     protected
     def set_campaign_pending
