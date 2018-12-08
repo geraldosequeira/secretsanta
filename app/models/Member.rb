24eebb6e090bddd94a50d80e58990f8c845c9cab
Geraldo Sequeira <gcm.sequeira@gmail.com>
2018-12-08 20:23:58 -0300

    for raffle

--- app/models/Member.rb
+++ app/models/Member.rb
@@ -8,11 +8,11 @@ class Member < ApplicationRecord
     def set_pixel
         self.open = false
         self.token = loop do
-            random_token = SecureRandom.urlsafe_base64(nil, false)
-            break random_token unless Member.exists?(token: random_token)
+          random_token = SecureRandom.urlsafe_base64(nil, false)
+          break random_token unless Member.exists?(token: random_token)
         end
         self.save!
-    end
+      end    
 
     protected
     def set_campaign_pending
